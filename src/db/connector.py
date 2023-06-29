912be423920bb649aaf8d84e7dd7f3310ea75936
ntsaturov <tsaturov.n@gmail.com>
2023-06-29 21:56:30 +0300

    Upd docker-compose and dockerfile

--- src/db/connector.py
+++ src/db/connector.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
